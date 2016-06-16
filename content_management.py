d1785a2ae0d6f9e4f9c90e1353caca1fc12c6284
ferbncode <suyashgargsfam@gmail.com>
2016-06-16 17:53:12 +0530

    updated content-management.py

--- content_management.py
+++ content_management.py
@@ -1,5 +1,4 @@
-#This file contains the basic content management system which judges what content is seen in the html pages
-import pickle
+import pickle#This file contains the basic content management system which judges what content is seen in the html pages
 def Content():
 	file1 = open('conman','rb')
 	DICT = pickle.load(file1)
@@ -10,11 +9,11 @@ def Database():
 	return DB
 def Write_Cont(CONT_DICT):
 	file1 = open('conman', 'wb')
-	file1.dump(CONT_DICT, file1)
+	file1.dump(CONT_DICT, file1, protocol=2)
 	file1.close()
 def Write_DB(DB):
 	file2 = open('dbbase', 'wb')
-	file2.dump(DB, file2)
+	file2.dump(DB, file2, protocol=2)
 	file2.close()
 
 	
